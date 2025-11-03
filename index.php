ff46c65bfbd381162c4e72095765669f1497b60e
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-11-04 00:02:43 +0700

    mencoba tailwind dengan cdn

--- index.php
+++ index.php
@@ -12,16 +12,22 @@
 </head>
 
 <body>
-    <h1>Cek Informasi Pajak Kendaraan Bermotor Online</h1>
-    <h1>
-        Cek pajak kendaraan bermotor online dengan mudah dan cepat 
-        menggunakan plat nomor dari berbagai provinsi di Indonesia. 
-        Dapatkan informasi status pajak, tanggal jatuh tempo, dan 
-        estimasi jumlah tagihan Anda. Cek melalui Layanan Informasi 
-        Resmi Samsat tiap provinsi untuk membantu memastikan kendaraan 
-        Anda tetap taat aturan dan terhindar dari denda.
-    </h1>
-    <button>Check</button>
+    <div class="w-full flex items-center justify-center h-screen">
+        <div class="max-w-[1080px] flex items-center justify-center">
+            <div class="max-w-[1080px] max-h-[1920px] bg-red-700">
+                <h1 class="text-[24px] font-bold">Cek Informasi Pajak Kendaraan Bermotor Online</h1>
+                <h1>
+                    Cek pajak kendaraan bermotor online dengan mudah dan cepat
+                    menggunakan plat nomor dari berbagai provinsi di Indonesia.
+                    Dapatkan informasi status pajak, tanggal jatuh tempo, dan
+                    estimasi jumlah tagihan Anda. Cek melalui Layanan Informasi
+                    Resmi Samsat tiap provinsi untuk membantu memastikan kendaraan
+                    Anda tetap taat aturan dan terhindar dari denda.
+                </h1>
+                <button>Check</button>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
