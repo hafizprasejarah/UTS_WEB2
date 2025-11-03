bf0149f89386afacd2a064cd1a9fd1052469e8f9
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-11-03 23:49:32 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pajak</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body>
+    <h1>Cek Informasi Pajak Kendaraan Bermotor Online</h1>
+    <h1>
+        Cek pajak kendaraan bermotor online dengan mudah dan cepat 
+        menggunakan plat nomor dari berbagai provinsi di Indonesia. 
+        Dapatkan informasi status pajak, tanggal jatuh tempo, dan 
+        estimasi jumlah tagihan Anda. Cek melalui Layanan Informasi 
+        Resmi Samsat tiap provinsi untuk membantu memastikan kendaraan 
+        Anda tetap taat aturan dan terhindar dari denda.
+    </h1>
+    <button>Check</button>
+</body>
+
+</html>
