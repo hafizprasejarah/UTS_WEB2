f3e1ada8781906ea7734f4597855a5ffb72cf28b
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-11-06 00:28:25 +0700

    Perbaikan tampilan 'belum selesai'

--- index.php
+++ index.php
@@ -8,15 +8,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pajak</title>
+    <style>
+        * {
+            font-family: "Lato", sans-serif;
+        }
+    </style>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
-<body>
-    <div class="w-full flex items-center justify-center h-screen">
-        <div class="max-w-[1080px] flex items-center justify-center">
-            <div class="max-w-[1080px] max-h-[1920px] bg-red-700">
+<body class="p-0 m-0">
+    <div class="w-full flex  justify-center ">
+        <div class="max-w-[1080px] flex justify-center py-6 shadow-xl">
+            <div class="max-w-[1080px] max-h-[1920px] shadow-lg bg-[#EEEEEE] p-6 rounded-md space-y-2">
                 <h1 class="text-[24px] font-bold">Cek Informasi Pajak Kendaraan Bermotor Online</h1>
-                <h1>
+                <h1 class="text-justify text-[18px]">
                     Cek pajak kendaraan bermotor online dengan mudah dan cepat
                     menggunakan plat nomor dari berbagai provinsi di Indonesia.
                     Dapatkan informasi status pajak, tanggal jatuh tempo, dan
@@ -24,8 +31,50 @@
                     Resmi Samsat tiap provinsi untuk membantu memastikan kendaraan
                     Anda tetap taat aturan dan terhindar dari denda.
                 </h1>
-                <button>Check</button>
+
+                <div class="flex space-x-2 text-[24px] my-6">
+                    <a href="" class=" bg-blue-500 px-4 py-2 rounded-[10px] text-white hover:bg-blue-600 hover:text-gray-200 transition">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                    <a href="" class="bg-blue-500 px-4 py-2 rounded-[10px] text-white hover:bg-blue-600 hover:text-gray-200 transition">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="" class="bg-blue-500 px-4 py-2 rounded-[10px] text-white hover:bg-blue-600 hover:text-gray-200 transition">
+                        <i class="fa-brands fa-linkedin-in"></i>
+
+                    </a>
+                    <a href="" class="bg-blue-500 px-4 py-2 rounded-[10px] text-white hover:bg-blue-600 hover:text-gray-200 transition">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </div>
+
+                <div class="grid grid-cols-1 text-[20px] font-bold">
+                    <div>
+                        <div>Jawa</div>
+                        <ul class="text-[16px] font-normal pl-5 space-y-2">
+                            <li class="flex justify-between border-b py-4">
+                                <div>
+                                    <h4 class="font-semibold">Cek Pajak Kendaraan DIY Yogyakarta</h4>
+                                    <div class="text-[14px]">Plat Nomor:Sleman, Bantul, Gunungkidul, Kulon Progo, Yogyakarta</div>
+                                </div>
+                                <i class="fa-solid fa-chevron-right"></i>
+                            </li>
+                            <li>Cek Pajak Kendaraan DKI Jakarta</li>
+                            <li>Cek Pajak Kendaraan Jawa Barat</li>
+                            <li>Cek Pajak Kendaraan Jawa Tengah</li>
+                            <li>Cek Pajak Kendaraan Jawa Timur</li>
+                        </ul>
+                    </div>
+                    <div>Sumatra</div>
+                    <div>Kalimantan</div>
+                    <div>Sulawesi</div>
+                    <div>Papua</div>
+                    <div>Bali & NTT</div>
+                    <div>Maluku</div>
+
+                </div>
             </div>
+
         </div>
     </div>
 </body>
